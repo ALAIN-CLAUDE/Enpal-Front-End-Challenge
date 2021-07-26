b927b5e2098bbd50b6681686a35e6d4ce7a49c88
Dontee930 <63712848+Dontee930@users.noreply.github.com>
2021-07-26 10:50:46 +0300

    Update login_to_org.sh

--- build/login_to_org.sh
+++ build/login_to_org.sh
@@ -3,4 +3,4 @@ mkdir keys
 echo $SFDC_SERVER_KEY | base64 -di > keys/server.key
 
 echo "Authenticating org"
-sfdx force:auth:jwt:grant --clientid $APP_KEY --jwtkeyfile keys/server.key --username $SF_USERNAME --instanceurl 	https://creative-goat-9jgax0-dev-ed.my.salesforce.com 
+sfdx force:auth:jwt:grant --clientid $APP_KEY --jwtkeyfile keys/server.key --username $SF_USERNAME --instanceurl 	$ENDPOINT
